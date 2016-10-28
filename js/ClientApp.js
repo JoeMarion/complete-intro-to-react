7d91b7e99e0f944de7714dd5b322a55ccac9fe74
Joe Marion <joemarion.io@gmail.com>
2016-10-28 16:48:50 -0700

    Integrate webpack.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,24 +1,16 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
+var MyTitle = require('./MyTitle')
 
 var MyTitleFact = React.createFactory(MyTitle)
-var ec = React.createElement
+var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    React.createElement(MyTitle, {title: 'Props are great', color: 'rebeccapurple'}),
-    ec(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
-    MyTitleFact({title: 'Props are the best!', color: 'papayawhip'})
+    MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
+    ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
   )
 )
 
